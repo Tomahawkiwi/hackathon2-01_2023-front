53a9d7c7c28025afb02e5246a7ab6bac0e64356c
Les Crow <john.baruteau@gmail.com>
2023-01-12 00:41:04 +0100

    get one

--- pages/api/cars/[id].tsx
+++ pages/api/cars/[id].tsx
@@ -1,7 +1,28 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
 
-const handler = (req: NextApiRequest, res: NextApiRequest) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method, query } = req;
+  const { id } = req.query;
+
+  let car;
+  switch (method) {
+    case "GET":
+      car = await prisma.car.findUniqueOrThrow({
+        where: {
+          id: id as string,
+        },
+      });
+
+      res.status(200).json(car);
+
+      break;
+    case "POST":
+      res.status(200).json({ message: "POST" });
+      break;
+    default:
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 };
 
 export default handler;
