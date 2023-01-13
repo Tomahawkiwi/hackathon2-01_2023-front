edf28e870d048af4d43da0067a8571406cc785ef
Damsbot64 <damien64durand@gmail.com>
2023-01-13 01:35:08 +0100

    calendar works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ChooseOption from "../src/components/ChooseOption";
+import Calendar from "../src/components/Calendar";
 
 function Index() {
   const [openOption, setOpenOption] = useState(true);
